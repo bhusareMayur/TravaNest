48a2eb372e8379eb1ff7f06338dd8c5bed688d55
Mayur Bhusare <mbhusare_cs@jspmrscoe.edu.in>
2025-05-28 12:25:42 +0530

    Add initials

---
+++ routes/listing.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const expressError = require("../utils/expressError.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { cloudinary, storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single('listing[image]') ,
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+
+//new route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single('listing[image]') ,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+//edit route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+module.exports = router;
