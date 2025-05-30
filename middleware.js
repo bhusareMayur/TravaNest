de0f20b039173d68056930e9ae7ba3f78fb0fa09
Mayur Bhusare <mayurbhusare8262@gmail.com>
2025-05-30 21:07:48 +0530

    review authentication

--- middleware.js
+++ middleware.js
@@ -51,13 +51,30 @@ module.exports.validateReview  = (req, res, next) => {
     }
   };
   
-module.exports.isReviewAuthor = async(req, res, next) => {
-  let {id , reviewId } = req.params;
-  let review = await Review.findById(reviewId);
-    if(!review.author._id.equals(res.locals.currUser._id)){
-      req.flash("error", "You are not the author of this review!");
-      return res.redirect(`/listings/${id}`);
+// module.exports.isReviewAuthor = async(req, res, next) => {
+//   let {id , reviewId } = req.params;
+//   let review = await Review.findById(reviewId);
+//     if(!review.author._id.equals(res.locals.currUser._id)){
+//       req.flash("error", "You are not the author of this review!");
+//       return res.redirect(`/listings/${id}`);
 
-    }
+//     }
+//   next();
+// };
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review) {
+    req.flash("error", "Review not found!");
+    return res.redirect(`/listings/${id}`);
+  }
+
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You are not the author of this review!");
+    return res.redirect(`/listings/${id}`);
+  }
+
   next();
 };
+
